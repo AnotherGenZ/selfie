63935888eea14a085f0da1932aa252495a323059
Motion <motiond3v@users.noreply.github.com>
2017-01-15 18:49:44 -0600

    Delete self.js

--- self.js
+++
@@ -1,9 +0,0 @@
-var Discord = require("discord.js");
-global.bot = new Discord.Client();
-var handler = require("./handler");
-
-bot.on("ready", () => {
-    handler(bot);
-});
-
-bot.login("user-token-here");
